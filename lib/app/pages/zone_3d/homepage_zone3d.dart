d7dec4cbe92dc2098fbd2e18803aa7c1c6e8b83e
xElGrego <elgrego977@gmail.com>
2021-07-26 20:10:42 -0500

    Add 3d zone

---
+++ lib/app/pages/zone_3d/homepage_zone3d.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_tesis/app/pages/zone_3d/planets/planets_3d.dart';
+
+import 'bacterias/bacterias_3d.dart';
+
+class HomePageZone3d extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Planets3D(),
+                  ),
+                );
+              },
+              child: Text('Planetas 3D'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Bacteries3d(),
+                  ),
+                );
+              },
+              child: Text('Bacterias 3d'),
+            ),
+          ],
+        ),
+      );
+  }
+}
